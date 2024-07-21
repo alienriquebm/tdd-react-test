7adaacd09f25680011b987c6addb71b2abaf2153
Ali Briceno <alienriquebm@gmail.com>
2024-07-21 00:25:36 -0400

    added storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from 'react-aria-components';
+
 function App() {
-  return <div className="text-xl text-slate-600">Test</div>;
+  return (
+    <div className="text-xl text-slate-600">
+      <Button onPress={() => alert('Hello world!')}>Press me</Button>
+    </div>
+  );
 }
 
 export default App;
